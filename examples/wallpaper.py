6061b1054e55bd1de8d33fa0eeebadf7f7f60cff
MuedeHydra <tiger12@gmx.ch>
2024-08-18 23:51:21 +0200

    add wallpaper.py

---
+++ examples/wallpaper.py
@@ -0,0 +1,72 @@
+#!/bin/python3
+
+import random
+import os
+import sys
+
+wallpaper_path = "/home/tzwicker/Dokumente/python/anime-dlp-2/wallpaper"
+wallpaper_old = "wallpaper_old.txt"
+pre_args: str = "-x .csv --input $HOME/Dokumente/python/anime-dlp-2/tags.csv"
+
+
+def main(args_str: str):
+
+    get_img: str = os.popen(f"cft {args_str}").read()
+
+    img_list: list = get_img.splitlines()
+
+    try:
+        with open(wallpaper_old, "r", encoding="utf-8") as file:
+            wallpaper_old_img: str = file.readline()
+
+    except FileNotFoundError:
+        os.system(f"touch {wallpaper_old}")
+
+    number: int = len(img_list) - 1
+    pick_img: int = random.randint(0, number)
+
+    if img_list[pick_img] == wallpaper_old_img:
+        pick_img += 1
+
+    if pick_img > number:
+        pick_img = 0
+
+    print(f"{wallpaper_path}/{img_list[pick_img]}")
+    os.system(f'swww img "{wallpaper_path}/{img_list[pick_img]}"')
+
+    with open(wallpaper_old, "w", encoding="utf-8") as file:
+        file.writelines(img_list[pick_img])
+
+
+def tofi() -> str:
+    comand: str = os.popen('echo -e "any\nand\nnot\ncostum" | tofi'
+                           ).read().removesuffix("\n")
+    if comand != "costum":
+        tag: str = os.popen(f"echo $(cft {pre_args} --list-tags |"
+                            f"tofi --prompt-text '{comand} ')"
+                            ).read().removesuffix("\n")
+        tag = tag[tag.find("] ") + 2:tag.find(" (")]
+        return f'--{comand} "{tag}"'
+    else:
+        return os.popen("tofi --require-match=false").read().removesuffix("\n")
+
+
+def args_func(args: list) -> str:
+    args_str: str = ""
+    for i in args:
+        args_str += i
+        args_str += " "
+    return args_str
+
+
+if __name__ == "__main__":
+    args: list = (sys.argv[1:])
+    if args == []:
+        args_str: str = tofi()
+    else:
+        args_str: str = args_func(args)
+
+    args_str = args_str.strip()
+    args_str += f" {pre_args}"
+
+    main(args_str)
